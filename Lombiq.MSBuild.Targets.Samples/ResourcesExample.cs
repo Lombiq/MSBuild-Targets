75519e326f2824a1c4d3f80f561c24fe68ecc491
Zoltán Lehóczky <zoltan.lehoczky@lombiq.com>
2025-12-09 01:35:04 +0100

    Typos

--- Lombiq.MSBuild.Targets.Samples/ResourcesExample.cs
+++ Lombiq.MSBuild.Targets.Samples/ResourcesExample.cs
@@ -1,9 +1,7 @@
-using Lombiq.HelpfulLibraries.Attributes;
-
 namespace Lombiq.MSBuild.Targets.Samples;
 
-// Here you can see, that the version strings from libman.json are exposed as static constants without any special
-// configuration. You just have to import Lombiq.MSBuild.LibMan.Targets and include the "[LibManVersions]" attronite.
+// Here you can see that the version strings from libman.json are exposed as static constants without any special
+// configuration. You just have to import Lombiq.MSBuild.LibMan.Targets and include the "[LibManVersions]" attribute.
 // (Also reference Lombiq.HelpfulLibraries.SourceGenerators and Lombiq.HelpfulLibraries.SourceGenerators if you are
 // using Lombiq.MSBuild.LibMan.Targets as a NuGet package.)
 
